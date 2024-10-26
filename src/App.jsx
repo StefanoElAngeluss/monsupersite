871af9a16dc02a6daa93b944a4cd81c679fc22c1
Stefano <stephandavy@outlook.fr>
2024-10-26 23:28:35 +0200

    ajout de la route et des pages.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Accueil from "./pages/Accueil"
+import About from "./pages/About"
+import Projets from "./pages/Projets"
+import Dashboard from "./pages/Dashboard"
+import Connexion from "./comptes/Connexion"
+import Inscription from "./comptes/Inscription"
+
 export default function App() {
   return (
-    <h1 className="text-4xl text-center text-rose-600 font-bold underline">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Accueil />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projets" element={<Projets />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/comptes/connexion" element={<Connexion />} />
+        <Route path="/comptes/inscription" element={<Inscription />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
---
+++ src/pages/Projets.jsx
@@ -0,0 +1,5 @@
+export default function Projets() {
+  return (
+    <div>Projets</div>
+  )
+}
